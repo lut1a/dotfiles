fc1a2cf567c9480da6304c26e455717cce3f521d
Lutia <117948140+lut1a@users.noreply.github.com>
2023-02-26 07:34:53 +0100

    Update bin/remotebackup.sh

--- bin/executable_remotebackup.sh
+++ bin/executable_remotebackup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rclone --bwlimit 70K:off -P sync $HOME/Documents/ gdrive1:/Documents/ -password-command="gopass rclone/config"
-rclone --bwlimit 70K:off -P sync $HOME/.pim/ gdrive1:/pim/ -password-command="gopass rclone/config"
-rclone --bwlimit 70K:off -P sync $HOME/Documents/ onedrive1:/Documents/ -password-command="gopass rclone/config"
-rclone --bwlimit 70K:off -P sync $HOME/Pictures/ onedrive1:/Pictures/ -password-command="gopass rclone/config"
-rclone --bwlimit 70K:off -P sync $HOME/.pim/ onedrive1:/pim/ -password-command="gopass rclone/config"
+rclone --bwlimit 70K:off -P sync $HOME/Documents/ gdrive1:/Documents/ --password-command="gopass rclone/config"
+rclone --bwlimit 70K:off -P sync $HOME/.pim/ gdrive1:/pim/ --password-command="gopass rclone/config"
+rclone --bwlimit 70K:off -P sync $HOME/Documents/ onedrive1:/Documents/ --password-command="gopass rclone/config"
+rclone --bwlimit 70K:off -P sync $HOME/Pictures/ onedrive1:/Pictures/ --password-command="gopass rclone/config"
+rclone --bwlimit 70K:off -P sync $HOME/.pim/ onedrive1:/pim/ --password-command="gopass rclone/config"
