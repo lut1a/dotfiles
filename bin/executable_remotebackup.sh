507403410320f33158d4649edd0c721a7d4440fb
Lutia <117948140+lut1a@users.noreply.github.com>
2023-02-26 07:36:24 +0100

    Update bin/remotebackup.sh

--- bin/executable_remotebackup.sh
+++ bin/executable_remotebackup.sh
@@ -5,4 +5,4 @@ rclone --bwlimit 70K:off -P sync $HOME/.pim/ gdrive1:/pim/ --password-command="g
 rclone --bwlimit 70K:off -P sync $HOME/Documents/ onedrive1:/Documents/ --password-command="gopass rclone/config"
 rclone --bwlimit 70K:off -P sync $HOME/Pictures/ onedrive1:/Pictures/ --password-command="gopass rclone/config"
 rclone --bwlimit 70K:off -P sync $HOME/.pim/ onedrive1:/pim/ --password-command="gopass rclone/config"
-chezmoi add $HOME/.config/rclone/config
+chezmoi add $HOME/.config/rclone/rclone.conf
