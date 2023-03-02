47f1a086372238d0f382a309ebb8a4552ca4dfce
Lutia <117948140+lut1a@users.noreply.github.com>
2023-03-02 18:49:29 +0100

    dotfiles update

--- bin/executable_itbkpsync.sh
+++ bin/executable_itbkpsync.sh
@@ -2,5 +2,5 @@
 
 rsync -avh --progress --exclude-from="$HOME"/bin/exclude.txt --delete-before "$HOME"/ /mnt/t5intbkp/"$USER"/
 rsync -avh --progress --exclude-from="$HOME"/bin/exclude.txt --delete-before "$HOME"/.pim/ /mnt/t5intbkp/.pim/
-rsync -avh --progress --exclude-from="$HOME"/bin/exclude.txt --delete-before "$HOME"/.local/share/chezmoi/ /mnt/t5intbkp/dotfiles/
+rsync -avh --progress --delete-before "$HOME"/.local/share/chezmoi/ /mnt/t5intbkp/dotfiles/
 rsync -avh --progress --exclude-from="$HOME"/bin/exclude.txt --delete-before /var/cache/pacman/ /mnt/t5intbkp/pacman/
