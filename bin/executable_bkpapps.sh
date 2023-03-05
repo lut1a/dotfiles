3dbe6b9ca1b2717c8bc8ae4ba08b1a725f50735b
Lutia <117948140+lut1a@users.noreply.github.com>
2023-03-05 07:35:56 +0100

    dotfiles update

--- bin/executable_bkpapps.sh
+++ bin/executable_bkpapps.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 comm -23 <(pacman -Qneq | sort) <(pacman -Qqg -g gnome | sort | uniq) |
 	grep -v 'radeon\|nvidia\|intel\|amd\|linux\|qt5\|gnome' |
-	tr '\n' ' ' >"$HOME"/bin/manuallyinstalledpkgs.txt
+	tr '\n' ' ' >"$HOME"/bin/archpkgs.txt
 
-pacman -Qmeq | tr '\n' ' ' >"$HOME"/bin/aurpackages.txt
-#flatpak list --columns=application --app > "$HOME"/bin/installedflatpaks.txt
-#brew leaves > "$HOME"/bin/brewpackages.txt
-chezmoi add "$HOME"/bin/manuallyinstalledpkgs.txt
+pacman -Qmeq | tr '\n' ' ' >"$HOME"/bin/aurpkgs.txt
+#flatpak list --columns=application --app > "$HOME"/bin/fltpkgs.txt
+#brew leaves > "$HOME"/bin/brewpkgs.txt
+chezmoi add "$HOME"/bin/archpkgs.txt
+chezmoi add "$HOME"/bin/aurpkgs.txt
