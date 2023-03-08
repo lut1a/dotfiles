8780afb07cd29ae682bf15f3001640a459239692
Lutia <117948140+lut1a@users.noreply.github.com>
2023-03-09 00:56:57 +0100

    dotfiles update

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,18 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  "Mofiqul/vscode.nvim",
+})
 vim.opt.encoding="utf-8"
 vim.g.mapleader = ","
 HOME = os.getenv('HOME')
@@ -15,3 +30,9 @@ vim.keymap.set({'n', 'x'}, 'cp', '"+y')
 vim.keymap.set({'n', 'x'}, 'cv', '"+p')
 vim.keymap.set({'n', 'x'}, 'x', '"_x')
 vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+
+
+vim.opt.termguicolors = true
+vim.o.background = 'light'
+vim.cmd.colorscheme('vscode')
