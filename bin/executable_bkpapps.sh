cd5cd3dc4e3e8c1eb618a2cfdb61b71e8fc02b7d
Lutia <117948140+lut1a@users.noreply.github.com>
2023-03-11 13:09:46 +0100

    dotfiles update

--- bin/executable_bkpapps.sh
+++ bin/executable_bkpapps.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 comm -23 <(pacman -Qneq | sort) <(pacman -Qqg -g gnome | sort | uniq) |
-	grep -v 'radeon\|nvidia\|intel\|amd\|linux\|qt5\|gnome' |
-	tr '\n' ' ' >"$HOME"/bin/archpkgs.txt
+	grep -v 'radeon\|nvidia\|intel\|amd\|linux\|qt5\|gnome' >"$HOME"/bin/archpkgs.txt
 
-pacman -Qmeq | tr '\n' ' ' >"$HOME"/bin/aurpkgs.txt
+pacman -Qmeq >"$HOME"/bin/aurpkgs.txt
 #flatpak list --columns=application --app > "$HOME"/bin/fltpkgs.txt
 #brew leaves > "$HOME"/bin/brewpkgs.txt
 chezmoi add "$HOME"/bin/archpkgs.txt
--- bin/executable_cleanpkg.sh
+++ bin/executable_cleanpkg.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 pacman -Qtdq | sudo pacman -Rns -
+echo "more stuff to delete, caution necessary"
+pacman -Qqd | pacman -Rsu --print -
 sudo paccache -ruk0
 sudo paccache -rk3
 #flatpak remove --unused
