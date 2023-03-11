a53c84d2d7b0cf77ec81ff4b98d9a195cc44e9d3
Lutia <117948140+lut1a@users.noreply.github.com>
2023-03-11 13:17:05 +0100

    dotfiles update

--- bin/executable_mirrordeletions.sh
+++ bin/executable_mirrordeletions.sh
@@ -5,11 +5,12 @@ comm -23 <(pacman -Qneq | sort) <(pacman -Qqg -g gnome | sort | uniq) |
 	grep -v 'radeon\|nvidia\|intel\|amd\|linux\|qt5\|gnome' >/run/user/"$UID"/mirrord/archpkgs.txt
 
 pacman -Qmeq >/run/user/"$UID"/mirrord/aurpkgs.txt
-comm -13 "$HOME"/bin/archpkgs.txt /run/user/"$UID"/mirrord/archpkgs.txt | less
-comm -13 "$HOME"/bin/aurpkgs.txt /run/user/"$UID"/mirrord/aurpkgs.txt | less
-#xargs -a /run/user/"$UID"/mirrord/archpkgs.txt sudo pacman -Rus
+comm -13 "$HOME"/bin/archpkgs.txt /run/user/"$UID"/mirrord/archpkgs.txt > /run/user/"$UID"/mirrord/archpkgs2.txt
+xargs -a /run/user/"$UID"/mirrord/archpkgs2.txt sudo pacman -Rus
+comm -13 "$HOME"/bin/aurpkgs.txt /run/user/"$UID"/mirrord/aurpkgs.txt > /run/user/"$UID"/mirrord/aurpkgs2.txt
+xargs -a /run/user/"$UID"/mirrord/aurpkgs2.txt sudo pacman -Rus
 rm /run/user/"$UID"/mirrord/archpkgs.txt
 rm /run/user/"$UID"/mirrord/aurpkgs.txt
+rm /run/user/"$UID"/mirrord/archpkgs2.txt
+rm /run/user/"$UID"/mirrord/aurpkgs2.txt
 rmdir /run/user/"$UID"/mirrord/
-#flatpak list --columns=application --app > "$HOME"/bin/fltpkgs.txt
-#brew leaves > "$HOME"/bin/brewpkgs.txt
