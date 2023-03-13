2c52e07d01f77c7024e169941d3c386fc50e775b
lut1a <117948140+lut1a@users.noreply.github.com>
2023-03-13 23:51:12 +0100

    dotfiles update

--- bin/executable_etcbkp.sh
+++ bin/executable_etcbkp.sh
@@ -5,10 +5,6 @@ mkdir -pv "$HOME"/etcbkp/systemd/journald.conf.d/
 mkdir -pv "$HOME"/etcbkp/systemd/resolved.conf.d/
 mkdir -pv "$HOME"/etcbkp/systemd/system.conf.d/
 mkdir -pv "$HOME"/etcbkp/udev/rules.d/
-mkdir -pv "$HOME"/etcbkp/NetworkManager/conf.d/
-mkdir -pv "$HOME"/etcbkp/iwd/
-mkdir -pv "$HOME"/etcbkp/kernel/
-mkdir -pv "$HOME"/etcbkp/mkinitcpio.d/
 mkdir -pv "$HOME"/etcbkp/modprobe.d/
 
 cp /etc/locale.conf "$HOME"/etcbkp/locale.conf
@@ -23,13 +19,6 @@ cp /etc/systemd/zram-generator.conf "$HOME"/etcbkp/systemd/zram-generator.conf
 cp /etc/systemd/system/batterythreshold.service "$HOME"/etcbkp/systemd/system/batterythreshold.service
 cp /etc/vconsole.conf "$HOME"/etcbkp/vconsole.conf
 cp /etc/udev/rules.d/* "$HOME"/etcbkp/udev/rules.d/
-cp /etc/NetworkManager/conf.d/wifi_backend.conf "$HOME"/etcbkp/NetworkManager/conf.d/wifi_backend.conf
-cp /etc/NetworkManager/conf.d/dns.conf "$HOME"/etcbkp/NetworkManager/conf.d/dns.conf
-cp /etc/iwd/main.conf "$HOME"/etcbkp/iwd/main.conf
-cp /etc/kernel/cmdline "$HOME"/etcbkp/kernel/cmdline
-cp /etc/mkinitcpio.conf "$HOME"/etcbkp/mkinitcpio.conf
-cp /etc/mkinitcpio.d/* "$HOME"/etcbkp/mkinitcpio.d/
 cp /etc/modprobe.d/nouveau_blacklist.conf "$HOME"/etcbkp/modprobe.d/nouveau_blacklist.conf
-cp /etc/modprobe.d/sp1500_tco.conf "$HOME"/etcbkp/modprobe.d/sp1500_tco.conf
 
 chezmoi add "$HOME"/etcbkp/
