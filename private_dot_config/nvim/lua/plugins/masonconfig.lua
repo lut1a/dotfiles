317e0a774c397fb6cf7a0cb253eb5a28983909c0
Lutia <117948140+lut1a@users.noreply.github.com>
2023-03-13 15:03:56 +0100

    dotfiles update

---
+++ private_dot_config/nvim/lua/plugins/masonconfig.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+}
