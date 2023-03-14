d4aa8d1464c5841f2f878b8be26d50d057e28c0d
lut1a <117948140+lut1a@users.noreply.github.com>
2023-03-14 02:29:49 +0100

    dotfiles update

--- bin/executable_etcbkp.sh
+++ bin/executable_etcbkp.sh
@@ -15,7 +15,6 @@ cp /etc/pacman.d/mirrorlist "$HOME"/etcbkp/pacman.d/mirrorlist
 cp /etc/systemd/journald.conf.d/volatile.conf "$HOME"/etcbkp/systemd/journald.conf.d/volatile.conf
 cp /etc/systemd/system.conf.d/timeout.conf "$HOME"/etcbkp/systemd/system.conf.d/timeout.conf
 cp /etc/systemd/resolved.conf.d/cloudflare.conf "$HOME"/etcbkp/systemd/resolved.conf.d/cloudflare.conf
-cp /etc/systemd/zram-generator.conf "$HOME"/etcbkp/systemd/zram-generator.conf
 cp /etc/systemd/system/batterythreshold.service "$HOME"/etcbkp/systemd/system/batterythreshold.service
 cp /etc/vconsole.conf "$HOME"/etcbkp/vconsole.conf
 cp /etc/udev/rules.d/* "$HOME"/etcbkp/udev/rules.d/
